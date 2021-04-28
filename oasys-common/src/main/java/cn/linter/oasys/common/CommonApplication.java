2159d3454775be3f514078c929888f5d0d4614bf
zhaoqianqian1 <zhaoqianqian1@xiaomi.com>
2021-04-28 11:14:41 +0800

    修改common

---
+++ oasys-common/src/main/java/cn/linter/oasys/common/CommonApplication.java
@@ -0,0 +1,13 @@
+package cn.linter.oasys.common;
+
+/**
+ * @author zhaoqianqian1
+ * @version 1.0.0
+ * @createTime 2021-04-28 11:11:00
+ * @description TODO
+ */
+public class CommonApplication {
+    public static void main(String[] args) {
+
+    }
+}
